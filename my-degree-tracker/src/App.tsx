0b15e9e7001d60061a121af0898e058bdb420ae3
Aviad Zer <aviad1486@gmail.com>
2025-08-07 13:53:25 +0300

    modify app.tsx to student table

---
+++ my-degree-tracker/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentTable from './components/StudentTable';
+
+const App = () => {
+  return (
+    <div>
+      <h1>מעקב תואר</h1>
+      <StudentTable />
+    </div>
+  );
+};
+
+export default App;
