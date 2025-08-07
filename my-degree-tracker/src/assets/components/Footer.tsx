72c6eb1c211f27c28fad3386f5b9bf7469d09f52
NoamCohen54 <noamfvi423@gmail.com>
2025-08-07 13:15:54 +0300

    added Footer and Header

---
+++ my-degree-tracker/src/assets/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ background: '#f1f1f1', padding: '1rem', textAlign: 'center' }}>
+      <p>Degree Status Management System 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ my-degree-tracker/src/assets/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ background: '#0077cc', color: 'white', padding: '1rem' }}>
+      <h1>Degree Tracker System</h1>
+    </header>
+  );
+};
+
+export default Header;
