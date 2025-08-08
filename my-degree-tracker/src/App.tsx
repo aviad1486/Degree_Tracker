1bd9168c988886fa907f296badd57343bba8f856
Aviad Zer <aviad1486@gmail.com>
2025-08-08 12:02:56 +0300

    wiring up navigation

--- my-degree-tracker/src/App.tsx
+++ my-degree-tracker/src/App.tsx
@@ -1,13 +1,108 @@
 import React from 'react';
-import StudentTable from './components/StudentTable';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Box
+} from '@mui/material';
 
-const App = () => {
+import StudentForm from './Forms/StudentForm';
+import StudentList from './Forms/StudentList';
+import CourseForm from './Forms/CourseForm';
+import CourseList from './Forms/CourseList';
+import StudentCourseForm from './Forms/StudentCourseForm';
+import StudentCourseList from './Forms/StudentCourseList';
+import ProgramForm from './Forms/ProgramForm';
+import ProgramList from './Forms/ProgramList';
+
+const drawerWidth = 240;
+
+export default function App() {
   return (
-    <div>
-      <h1>Degree Tracker</h1>
-      <StudentTable />
-    </div>
-  );
-};
+    <BrowserRouter>
+      <Box sx={{ display: 'flex' }}>
+        <AppBar position="fixed" sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Degree Tracker System
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-export default App;
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box' }
+          }}
+        >
+          <Toolbar />
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/students/new">
+                <ListItemText primary="Add Student" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/students">
+                <ListItemText primary="Student List" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/courses/new">
+                <ListItemText primary="Add Course" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/courses">
+                <ListItemText primary="Course List" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/student-courses/new">
+                <ListItemText primary="Add Student Course" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/student-courses">
+                <ListItemText primary="Student Course Records" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/programs/new">
+                <ListItemText primary="Add Program" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/programs">
+                <ListItemText primary="Program List" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Routes>
+            <Route path="/students/new" element={<StudentForm />} />
+            <Route path="/students" element={<StudentList />} />
+            <Route path="/courses/new" element={<CourseForm />} />
+            <Route path="/courses" element={<CourseList />} />
+            <Route path="/student-courses/new" element={<StudentCourseForm />} />
+            <Route path="/student-courses" element={<StudentCourseList />} />
+            <Route path="/programs/new" element={<ProgramForm />} />
+            <Route path="/programs" element={<ProgramList />} />
+            <Route path="*" element={<StudentList />} />
+          </Routes>
+        </Box>
+      </Box>
+    </BrowserRouter>
+  );
+}
