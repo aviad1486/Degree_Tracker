36806d915c438afc168e37a91eb1fe32bcbf1e1e
Aviad Zer <aviad1486@gmail.com>
2025-08-15 12:49:03 +0300

    Update HamburgerDrawer

--- my-degree-tracker/src/components/HamburgerDrawer.tsx
+++ my-degree-tracker/src/components/HamburgerDrawer.tsx
@@ -12,7 +12,6 @@ import {
   Assessment as AssessmentIcon,
   MenuBook as MenuBookIcon,
   TableChart as TableChartIcon,
-  People as PeopleIcon,
   Help as HelpIcon,
   ExitToApp as ExitToAppIcon,
 } from '@mui/icons-material';
@@ -30,49 +29,43 @@ export default function HamburgerDrawer({ onClose }: HamburgerDrawerProps) {
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/" onClick={onClose}>
             <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary="עמוד הבית" />
+            <ListItemText primary="Home Page" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><SchoolIcon /></ListItemIcon>
-            <ListItemText primary="ההתקדמות שלי" />
+            <ListItemText primary="My Progress" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><AssessmentIcon /></ListItemIcon>
-            <ListItemText primary="דו״ח ציונים" />
+            <ListItemText primary="Grade Report" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><MenuBookIcon /></ListItemIcon>
-            <ListItemText primary="קורסים" />
+            <ListItemText primary="Courses" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><TableChartIcon /></ListItemIcon>
-            <ListItemText primary="מסלול לימוד" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><PeopleIcon /></ListItemIcon>
-            <ListItemText primary="משתמשים" />
+            <ListItemText primary="Programs" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><HelpIcon /></ListItemIcon>
-            <ListItemText primary="עזרה והדרכה" />
+            <ListItemText primary="Help & Support" />
           </ListItemButton>
         </ListItem>
         <ListItem disablePadding>
           <ListItemButton>
             <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-            <ListItemText primary="התנתקות" />
+            <ListItemText primary="Logout" />
           </ListItemButton>
         </ListItem>
       </List>
