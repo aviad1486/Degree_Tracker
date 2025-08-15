3c4f7df2c5388cf1e3227984ef9e77c6933ebeb6
NoamCohen54 <152982236+NoamCohen54@users.noreply.github.com>
2025-08-15 11:53:23 +0300

    navigation to homepage from menu

--- my-degree-tracker/src/App.tsx
+++ my-degree-tracker/src/App.tsx
@@ -91,60 +91,64 @@ export default function App() {
 
   // Drawer של ההמבורגר - התפריט בעברית עם אייקונים
   const hamburgerDrawer = (
-    <div>
-      <Toolbar />
-      <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><HomeIcon /></ListItemIcon>
-            <ListItemText primary="עמוד הבית" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><SchoolIcon /></ListItemIcon>
-            <ListItemText primary="ההתקדמות שלי" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><AssessmentIcon /></ListItemIcon>
-            <ListItemText primary="דו״ח ציונים" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><MenuBookIcon /></ListItemIcon>
-            <ListItemText primary="קורסים" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><TableChartIcon /></ListItemIcon>
-            <ListItemText primary="מסלול לימוד" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><PeopleIcon /></ListItemIcon>
-            <ListItemText primary="משתמשים" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><HelpIcon /></ListItemIcon>
-            <ListItemText primary="עזרה והדרכה" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-            <ListItemText primary="התנתקות" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </div>
-  );
+  <div>
+    <Toolbar />
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton
+          component={Link}
+          to="/"
+          onClick={handleMenuToggle} // נסגור את התפריט אחרי מעבר
+        >
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText primary="עמוד הבית" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><SchoolIcon /></ListItemIcon>
+          <ListItemText primary="ההתקדמות שלי" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><AssessmentIcon /></ListItemIcon>
+          <ListItemText primary="דו״ח ציונים" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><MenuBookIcon /></ListItemIcon>
+          <ListItemText primary="קורסים" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><TableChartIcon /></ListItemIcon>
+          <ListItemText primary="מסלול לימוד" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><PeopleIcon /></ListItemIcon>
+          <ListItemText primary="משתמשים" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><HelpIcon /></ListItemIcon>
+          <ListItemText primary="עזרה והדרכה" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton>
+          <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+          <ListItemText primary="התנתקות" />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  </div>
+);
 
   return (
     <BrowserRouter>
