c4e4e60e5d0d070fcaa1b6ec7ab89b85f90df735
Aviad Zer <aviad1486@gmail.com>
2025-08-15 16:04:48 +0300

    adjust UI

--- my-degree-tracker/src/components/MainDrawer.tsx
+++ my-degree-tracker/src/components/MainDrawer.tsx
@@ -1,4 +1,21 @@
-import { List, ListItem, ListItemButton, ListItemText, Toolbar } from '@mui/material';
+import { 
+  List, 
+  ListItem, 
+  ListItemButton, 
+  ListItemIcon, 
+  ListItemText, 
+  Toolbar 
+} from '@mui/material';
+import { 
+  PersonAdd as PersonAddIcon,
+  People as PeopleIcon,
+  MenuBook as MenuBookIcon,
+  LibraryBooks as LibraryBooksIcon,
+  AddTask as AddTaskIcon,
+  Assignment as AssignmentIcon,
+  School as SchoolIcon,
+  TableChart as TableChartIcon
+} from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 export default function MainDrawer() {
@@ -8,41 +25,56 @@ export default function MainDrawer() {
       <List>
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/students/new">
+            <ListItemIcon><PersonAddIcon /></ListItemIcon>
             <ListItemText primary="Add Student" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/students">
+            <ListItemIcon><PeopleIcon /></ListItemIcon>
             <ListItemText primary="Student List" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/courses/new">
+            <ListItemIcon><MenuBookIcon /></ListItemIcon>
             <ListItemText primary="Add Course" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/courses">
+            <ListItemIcon><LibraryBooksIcon /></ListItemIcon>
             <ListItemText primary="Course List" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/student-courses/new">
+            <ListItemIcon><AddTaskIcon /></ListItemIcon>
             <ListItemText primary="Add Student Course" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/student-courses">
+            <ListItemIcon><AssignmentIcon /></ListItemIcon>
             <ListItemText primary="Student Course Records" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/programs/new">
+            <ListItemIcon><SchoolIcon /></ListItemIcon>
             <ListItemText primary="Add Program" />
           </ListItemButton>
         </ListItem>
+
         <ListItem disablePadding>
           <ListItemButton component={Link} to="/programs">
+            <ListItemIcon><TableChartIcon /></ListItemIcon>
             <ListItemText primary="Program List" />
           </ListItemButton>
         </ListItem>
