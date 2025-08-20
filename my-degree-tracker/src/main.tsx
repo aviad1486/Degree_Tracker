d0ffaf6edce0ac736d810d0300cce49c52191e67
NoamCohen54 <152982236+NoamCohen54@users.noreply.github.com>
2025-08-20 11:41:43 +0300

    Update main.tsx

--- my-degree-tracker/src/main.tsx
+++ my-degree-tracker/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { bootstrapLocalStorage } from './models/seed'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { bootstrapLocalStorage } from './models/seed';
 
 bootstrapLocalStorage(); // Seed initial data if there's no data
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
