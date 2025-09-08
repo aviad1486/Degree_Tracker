89842024878ecd49a09985fef4493ff7c6fc7a0d
Aviad Zer <aviad1486@gmail.com>
2025-09-08 16:51:56 +0300

    Fixing secret keys

--- my-degree-tracker/src/firestore/config.ts
+++ my-degree-tracker/src/firestore/config.ts
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
+// src/firestore/config.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFEri7AlTDFTjZZJJZ07Bwm3FW3kyYNHk",
-  authDomain: "degree-tracker-fe674.firebaseapp.com",
-  projectId: "degree-tracker-fe674",
-  storageBucket: "degree-tracker-fe674.firebasestorage.app",
-  messagingSenderId: "581428430033",
-  appId: "1:581428430033:web:c1f3ea1158d8d5cc2f71f0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(app);
